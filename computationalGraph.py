5ae146a961b7003ff12fa3a84cefcf6f74c4c020
Anirudh Nain <anain2@illinois.edu>
2019-04-29 01:44:54 -0500

    Cleaning Implementation

--- computationalGraph.py
+++ computationalGraph.py
@@ -1,81 +1,81 @@
+import operator
+
 global rootNodes 
 rootNodes = set()
 
-class TopologicalSort():
+class BatchProcess():
 
 	def __init__(self):
-		self.graph = {} #using graph for quick jumping #delete values before putting new resuls in graph
-		self.op = '*'
+		self.opToBeBatched = {operator.mul}
 
 	def processNode(self, node):
-		if node.val == self.op:
-			ans = 1
-			for child in node.child:
-				ans *= int(child.val)
+		#leafNode
+		if len(node.child) == 0:
+			return node.val
+		#Only single child
+		elif len(node.child) == 1:
+			return node.val(node.child.res)
+		#Multiple Child
+		else:
+			childern = list(node.child)
+			initalVal = node.val(childern[0].res, childern[1].res)
+			ans = initalVal
+			for i in range(2, len(childern)):
+				ans = node.val(res, childern[i].res)
 			return ans
 
 	def process(self, stack ,start, end):
 		for k in range(start,end):
 			#process the node
-			if stack[k].val == self.op: #convert self.op to a list and check self.ops.contains(self[k].val)
-				node = stack[k]
-				result = self.processNode(node)
-				#last parent won't be in graph so check
-				if stack[k] in self.graph:
-					parent = self.graph[stack[k]]
-					parent.child.remove(stack[k])
-					parent.child.add(Node(result))
-				else:
-					return result
+			node = stack[k]
+			if not node.res:
+				node.res = self.processNode(node)
 
-	def eval(self, stack):
+	def batchProcess(self, stack):
 		start = 0
 		for (i,elem) in enumerate(stack):
-			#eval all operations before it
-			if elem.val == self.op:
+			#eval all operations before this elem
+			if elem.val in self.opToBeBatched:
 				self.process(stack, start, i)
 				start = i
 			# print (num1.val, num2.val)
-		return self.process(stack,i,i+1)
+		self.process(stack,i,i+1)
 
-	#check for commands that are connection dependent and eval them
 	def topologicalSortUtil(self, node, visited, stack):
-		visited[node] = True
+		visited.add(node)
 		for child in node.child:
 			self.topologicalSortUtil(child, visited, stack)
-			self.graph[child] = node
 		stack.append(node) 
-	#call eval when you hit a node with connect op
 	
-	def topologicalSort(self):
-		visited = {}
+	def evaluate(self):
+		visited = set()
 		stack = []
 		global rootNodes
 		for node in rootNodes:
 			if node not in visited:
 				self.topologicalSortUtil(node, visited, stack)
-			print ("Print-Tree")
-			return self.eval(stack)
-			# stack = []
+			self.batchProcess(stack)
+			stack = []
 
-class Node():
+class NumpyBatchNode():
 
 	def __init__(self, val):
 		self.val = val
+		self.res = None
 		self.child = set() #can
 
 	def __mul__(self, other):
-		assert isinstance(other, Node) or isinstance(other, int) or isinstance(other, float)
-		if isinstance(other,Node):
-			return self.createComputationalTree(other, "*")
+		assert isinstance(other, NumpyBatchNode) or isinstance(other, int) or isinstance(other, float)
+		if isinstance(other,NumpyBatchNode):
+			return self.createComputationalTree(other, operator.mul)
 		else:
-			return self.createComputationalTree(Node(other), "*")
+			return self.createComputationalTree(NumpyBatchNode(other), operator.mul)
 
 	__rmul__ = __mul__
 
 	def createComputationalTree(self, node2, parent):
 
-		result = Node(parent)
+		result = NumpyBatchNode(parent)
 
 		global rootNodes
 
@@ -96,8 +96,8 @@ def printNode(node):
 		for child in node.child:
 			printNode (child)
 
-a = Node("4")
-b = Node("2")
+a = NumpyBatchNode(4)
+b = NumpyBatchNode(2)
 
 c = 5*b
 
@@ -105,19 +105,13 @@ d = 6*a
 
 k = c*d
 
-# e = 400*b
+r = c*d
 
-# print (d.val)
-# print ("Child")
-# printNode(d)
-# for elem in rootNodes:
-# 	printNode(elem)
+j = k*r
 
-t = TopologicalSort()
-print (t.topologicalSort())
+t = BatchProcess()
+t.evaluate()
 
-# n = Node("2")
-# k = n*3.0
-# print (k.val)
-
-# print ()
+print (k.res)
+print (r.res)
+print (j.res)
